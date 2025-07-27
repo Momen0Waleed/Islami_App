1ded0d4f6c36570d66852c4b5c87adf1849943a9
dell <mwgendia@gmail.com>
2025-07-27 09:32:38 +0300

    Finishing Quran Search V.2

--- lib/modules/quran/quran_view.dart
+++ lib/modules/quran/quran_view.dart
@@ -23,97 +23,104 @@ class _QuranViewState extends State<QuranView> {
     loadRecentSuraData();
   }
 
-  String searchQuery = "";
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(IslamiImages.backgroundQuranListPage),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          // spacing: 10,
-          children: [
-            Image.asset(IslamiImages.quranPageLogo),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              child: TextFormField(
-                onChanged: (value) {
-                  searchQuery = value;
-                  searchSura();
-                  setState(() {});
-                },
-                cursorColor: IslamiColors.gold,
-                decoration: InputDecoration(
-                  hintText: "Sura Name",
-                  hintStyle: Theme.of(context).textTheme.bodyLarge,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(color: IslamiColors.gold, width: 1),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(color: IslamiColors.gold, width: 1),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(color: IslamiColors.gold, width: 1),
-                  ),
+    return Stack(
+      children: [
+        Image.asset(IslamiImages.backgroundQuranListPage, fit: BoxFit.cover),
+        SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // spacing: 10,
+            children: [
+              Image.asset(IslamiImages.quranPageLogo),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 15,
+                ),
+                child: TextFormField(
+                  onChanged: (value) {
+                    searchQuery = value;
+                    searchSura();
+                    setState(() {});
+                  },
+                  cursorColor: IslamiColors.gold,
+                  decoration: InputDecoration(
+                    hintText: "Sura Name",
+                    hintStyle: Theme.of(context).textTheme.bodyLarge,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(
+                        color: IslamiColors.gold,
+                        width: 1,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(
+                        color: IslamiColors.gold,
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(
+                        color: IslamiColors.gold,
+                        width: 1,
+                      ),
+                    ),
 
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(9.0),
-                    child: ImageIcon(
-                      AssetImage(IslamiImages.mushafPageIcon),
-                      color: IslamiColors.gold,
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(9.0),
+                      child: ImageIcon(
+                        AssetImage(IslamiImages.mushafPageIcon),
+                        color: IslamiColors.gold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            Visibility(
-              visible: searchQuery.isEmpty,
-              replacement: SuraListWidget(
-                onSuraTab: onSuraTab,
-                suraDataModel: searchSuraList,
-              ),
-              child: Column(
-                children: [
-                  recentSuraList.isNotEmpty
-                      ? RecentlySuraWidget(
-                          suraDataList: recentSuraList,
-                          onSuraTap: onSuraTab,
-                        )
-                      : Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 10,
-                          ),
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              "No recent Sura yet",
-                              style: Theme.of(context).textTheme.bodyLarge!
-                                  .copyWith(color: IslamiColors.gold),
+              Visibility(
+                visible: searchQuery.isEmpty,
+                replacement: SuraListWidget(
+                  onSuraTab: onSuraTab,
+                  suraDataModel: searchSuraList,
+                ),
+                child: Column(
+                  children: [
+                    recentSuraList.isNotEmpty
+                        ? RecentlySuraWidget(
+                            suraDataList: recentSuraList,
+                            onSuraTap: onSuraTab,
+                          )
+                        : Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10,
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "No recent Sura yet",
+                                style: Theme.of(context).textTheme.bodyLarge!
+                                    .copyWith(color: IslamiColors.gold),
+                              ),
                             ),
                           ),
-                        ),
-                  SuraListWidget(
-                    onSuraTab: onSuraTab,
-                    suraDataModel: SuraConstraintList.suraData,
-                  ),
-                ],
+                    SuraListWidget(
+                      onSuraTab: onSuraTab,
+                      suraDataModel: SuraConstraintList.suraData,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 
@@ -158,6 +165,8 @@ class _QuranViewState extends State<QuranView> {
       recentSuraList.add(SuraConstraintList.suraData[indexInt]);
     }
   }
+
+  String searchQuery = "";
 
   List<SuraDataModel> searchSuraList = [];
 
